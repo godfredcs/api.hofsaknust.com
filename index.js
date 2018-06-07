4646d0b1615d3de3e11d5ae2df1ae8c404046fc6
godfredcs <addaiz.godfred@gmail.com>
2018-06-07 03:10:02 +0000

    Added socket.io but experiencing cors issues

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-require('dotenv-safe').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.set('port', process.env.PORT);
+const { PORT } = require('./src/config');
+const setHeaders = require('./src/middlewares/setHeaders');
+
+app.use(setHeaders);
+
+app.set('port', PORT);
+
+app.listen(app.get('port') || 5000, () => {
+    console.log('listening on ', PORT);
+});
 
 app.get('/', (req, res) => {
     res.send('hello how are you');
 });
 
-app.listen(app.get('port') || 3000);
+io.on('connection', socket => {
+    socket.emit('news', {
+        greeting: 'Saludos amigos'
+    });
+    
+    socket.on('received-post', data => {
+        console.log('The data from received-post is ', data);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('User disconnected');
+    })
+});
